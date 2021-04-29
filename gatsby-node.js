02d783cb8b87d7cc516b7ddbda586d0bad8fb33a
Derek Murr <dmurr@thrillworks.com>
2021-04-29 13:07:16 -0400

    eliminate redundant createPages calls

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,27 +24,33 @@ exports.createPages = async ({
     }
   `);
 
-  result.data?.allContentstackBlogPost.nodes.forEach(node => {
-    createPage({
-      path: `/monster${node.url}`,
-      component: path.resolve(`./src/templates/MonsterPage.tsx`),
-      context: {
-        pageId: node.id,
-        url: node.url,
-        locale: node.locale
-      },
+  result.data?.allContentstackBlogPost.nodes
+    .filter(node => node.locale === 'en-ca')
+    .forEach(node => {
+      console.log(`Creating page at /monster${node.url}`);
+      createPage({
+        path: `/monster${node.url}`,
+        component: path.resolve(`./src/templates/MonsterPage.tsx`),
+        context: {
+          pageId: node.id,
+          url: node.url,
+          locale: node.locale
+        },
+      });
     });
-  });
 
-  result.data?.allContentstackAuthor.nodes.forEach(node => {
-    createPage({
-      path: `/author${node.url}`,
-      component: path.resolve(`./src/templates/AuthorPage.tsx`),
-      context: {
-        pageId: node.id,
-        url: node.url,
-        locale: node.locale
-      },
+  result.data?.allContentstackAuthor.nodes
+    .filter(node => node.locale === 'en-ca')
+    .forEach(node => {
+      console.log(`Creating page at /author${node.url}`);
+      createPage({
+        path: `/author${node.url}`,
+        component: path.resolve(`./src/templates/AuthorPage.tsx`),
+        context: {
+          pageId: node.id,
+          url: node.url,
+          locale: node.locale
+        },
+      });
     });
-  });
 };
